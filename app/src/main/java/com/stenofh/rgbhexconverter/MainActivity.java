6686765fb22a1786fcc4bd51b1ce6cd9586219e8
Leonardo Nascimento <leonardo.stenofh@gmail.com>
2017-04-29 15:41:52 -0400

    Removed unused imports

--- app/src/main/java/com/stenofh/rgbhexconverter/MainActivity.java
+++ app/src/main/java/com/stenofh/rgbhexconverter/MainActivity.java
@@ -4,16 +4,13 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.Selection;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -21,7 +18,6 @@ import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import butterknife.OnTextChanged;
 
 public class MainActivity extends AppCompatActivity {
